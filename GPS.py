9fb816b37d1f22b63aa1a6061e8dbd0ba1bf9625
Karim Elawaad <karim.elawaad@hotmail.com>
2018-02-07 11:44:41 +0200

    bug fixes and added fix quality

--- GPS.py
+++ GPS.py
@@ -1,6 +1,7 @@
 import serial
 import time
 import os
+import GPS_readings
 
 class GPS:
 	def __init__(self, port='/dev/ttyAMA0', baudrate=9600):
@@ -9,34 +10,42 @@ class GPS:
 		self.ser = serial.Serial(port=self.port, baudrate=self.baudrate) 
 		self.initialized = False
 		self.current_milli_time = lambda: int(round(time.time() * 1000))
-
+                self.valid_coords = False
+                self.valid_satellites = False
+                
 	def attach(self):
 		if self.initialized is True:
 			return False
-		
-		self.port.write(str.encode('AT+CGNSPWR=1' + '/r/n')) #Power on the GPS antenna, should return OK
-		if wait_for_reply('OK/r/n', 2000) is not True:
+
+		self.ser.write(str.encode('AT+CGNSPWR=1' + '\r\n')) #Power on the GPS antenna, should return OK
+		time.sleep(1)
+		if self.wait_for_reply('OK\r\n', 2000) is not True:
+                        print('Not OK')                
 			return False
+		#print('power on')
 		
-		self.port.write(str.encode('AT+CGNSTST=1' + '/r/n'))
-		if wait_for_reply('OK/r/n', 2000) is not True:
+		self.ser.write(str.encode('AT+CGNSTST=1' + '\r\n'))
+		time.sleep(1)
+		if self.wait_for_reply('OK\r\n', 2000) is not True:
 			return False
 		
 
 		self.initialized = True
+		return True
 
 	def detach(self):
-		self.port.write(str.encode('AT+CGNSPWR=0' + '/r/n')) #Power off the GPS antenna, should return OK
+		self.ser.write(str.encode('AT+CGNSPWR=0' + '\r\n')) #Power off the GPS antenna, should return OK
 		self.initialized = False
 
 	def get_readings(self):
 		if self.ser.in_waiting:
 			received_nmea = self.ser.readline()
-
-			tokens = received_nmea.split(',')
-			readings = None
-			# If the sentence is a RMC sentence and the data is valid
+                        print(received_nmea)
+                        tokens = received_nmea.split(',')
+                        readings = None
+                        # If the sentence is a RMC sentence and the data is valid
 			if tokens[0] == '$GPRMC':
+                                self.valid_satellites = False
 				if tokens[1] != '':
 					time = tokens[1]
 					hour = time[:2]
@@ -49,10 +58,11 @@ class GPS:
 					month = date[2:4]
 					year = date[4:]
 
-					readings = GPS_readings(hour=hour,minutes=minutes, seconds=seconds, centiseconds=centiseconds,
+					readings = GPS_readings.GPS_readings(hour=hour,minutes=minutes, seconds=seconds, centiseconds=centiseconds,
 				 							day=day, month=month, year=year)
 				
 				if tokens[2] == 'A':
+                                        
 					latitude = tokens[3]
 					latitude_direction = tokens[4]
 
@@ -60,14 +70,29 @@ class GPS:
 					longitude_direction = tokens[6]
 
 					readings.latitude = latitude
-					readings.latitude_direction = latitude_direction
+					readings.latitude_dir = latitude_direction
 					readings.longitude = longitude
-					readings.longitude_direction = longitude_direction
+					readings.longitude_dir = longitude_direction
+                                        self.valid_coords = True
+
+			elif tokens[0] == '$GPGSV':
+				self.valid_satellites = True
+				self.valid_coords = False
+				num_satellites = tokens[3]
+				if readings is not None:
+                                    readings.num_satellites = num_satellites
+                                else:
+                                    readings = GPS_readings.GPS_readings(num_satellites=num_satellites)
+            elif tokens[0] == '$GPGGA':
+            	fix_quality = tokens[4]
+            	if fix_quality == '0':
+            		fix_quality = 'Invalid'
+            	elif fix_quality == '1':
+            		fix_quality = 'GPS Fix'
+        		elif fix_quality == '2':
+        			fix_quality = 'DGPS Fix'
 
 
-			if tokens[0] == '$GPGSV':
-				num_satellites = tokens[3] 
-				readings.num_satellites = num_satellites
 
 			return readings
 
@@ -76,8 +101,10 @@ class GPS:
 
 
 	def wait_for_reply(self, reply, timeout):
+ 
 		start_time = self.current_milli_time()
 		while (self.current_milli_time() - start_time) < timeout:
+    
 			if self.ser.in_waiting:
 				line = self.ser.readline()
 				if line == reply:
